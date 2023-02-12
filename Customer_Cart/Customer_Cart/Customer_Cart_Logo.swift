ba83d759f74110384290e1da8c275df65d050826
DpRuslan <yarkun.ruslan@gmail.com>
2023-02-12 20:25:32 +0100

    Add SearchBar

--- Customer_Cart/Customer_Cart/Customer_Cart_Logo.swift
+++ Customer_Cart/Customer_Cart/Customer_Cart_Logo.swift
@@ -1,13 +1,16 @@
 import UIKit
 
 class Customer_Cart_Logo: UIViewController {
+    var managerAPI: ManagerAPI = .shared
     @IBOutlet var logo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         logo.image = UIImage(named: "Custom_Cart")
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "Customer_Cart") as! Customer_Cart
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
-            self.navigationController?.pushViewController(vc, animated: true)
+        managerAPI.receiveParse { [weak self] in
+            DispatchQueue.main.async {
+                self?.navigationController?.pushViewController(vc, animated: true)
+            }
         }
     }
 }
